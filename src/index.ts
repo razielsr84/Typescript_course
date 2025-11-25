6c9c7a79b9e9d050a7aa8ce0a0c0045210f9c0a6
razielsr84 <raziel.ivan.sr84@gmail.com>
2025-11-25 14:36:25 -0600

    Primra update

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import type { User } from "./models/user.model";
+
+let user: User = {
+  name: "Juan",
+  lastName: "Perez",
+  age: 20,
+};
+
+const saludo: string = `La persona se llama ${user.name} ${user.lastName} y tiene ${user.age} años`;
+console.log("Hola mira esto :D ->");
+console.log("$" + saludo);
